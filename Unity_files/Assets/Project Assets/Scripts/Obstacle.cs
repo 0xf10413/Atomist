ebfe883fc60144d23b6730ef7a5a2f57f2edff74
flodeu <nintendocatz@msn.com>
2015-02-25 16:32:21 +0100

    Ajout de la classe obstacle.
    Ajout de la classe obstacle à l'ensemble des classes.

---
+++ Unity_files/Assets/Project Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Symbolise un type d'obstacle. Contient les informations liées :
+/// nom, ressource graphique.
+/// </summary>
+public class Obstacle
+{
+    public string name { get; private set; } // Le nom de l'obstacle (e.g. "Feu")
+    public Sprite obstacleResource { get; private set; } // Ressource graphique
+
+    /// <summary>
+    /// Constructeur de l'obstacle.
+    /// </summary>
+    /// <param name="nName">Le nom de l'obstacle.</param>
+    /// <param name="fileName">Le nom du fichier image (sans extension).</param>
+    public Obstacle (string nName, string fileName)
+    {
+        name = nName;
+        obstacleResource = Resources.Load<Sprite> ("Images/Obstacles/" + fileName);
+    }
+}
