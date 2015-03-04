f8fd427be1e643d5db8a60198525a3ecc872b3f1
flodeu <nintendocatz@msn.com>
2015-03-04 14:46:42 +0100

    Ajout de ObstacleToken
    Ajout de la classe de symbole d'obstacle.

---
+++ Unity_files/Assets/Project Assets/Scripts/ObstacleToken.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// Symbolise un jeton d'obstacle. Contient un GameObject, et un lien vers
+/// l'obstacle qu'il représente.
+/// </summary>
+public class ObstacleToken
+{
+    public Obstacle obstacle { get; private set; } // L'obstacle représenté
+    public GameObject obstacleImg { get; private set; } // Le GameObject correspondant
+
+    /// <summary>
+    /// Constructeur du jeton d'obstacle.
+    /// </summary>
+    /// <param name="nObstacle">L'obstacle référencé.</param>
+    public ObstacleToken (Obstacle nObstacle)
+    {
+        obstacle = nObstacle;
+        /* Initialisation du GameObject */
+        obstacleImg = new GameObject ();
+        obstacleImg.name = "Token " + nObstacle.name;
+        obstacleImg.AddComponent<Image> ();
+        obstacleImg.GetComponent<Image> ().sprite = nObstacle.obstacleResource;
+
+    }
+}
